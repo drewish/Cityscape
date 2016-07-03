e02421a278de94ab041511510074b07d63b7bff1
andrew morton <drewish@katherinehouse.com>
2016-07-03 14:52:47 -0600

    Moved Vehicle to its own class

---
+++ include/Vehicle.h
@@ -0,0 +1,63 @@
+//
+//  Vehicle.hpp
+//  Cityscape
+//
+//  Created by andrew morton on 7/3/16.
+//
+//
+
+#pragma once
+
+// TODO:
+// - interpolate the rotation angle turning
+// - only closed polylines should loop 
+// - move this to its own files
+// - draw a custom shape to indicate direction
+// - build path from highways in city
+// - use instanced drawing
+// - move into the citydata structure
+class Vehicle {
+  public:
+
+    void setup( const ci::PolyLine2f &path );
+    void update( double dt );
+    void draw() const;
+
+    ci::vec2 getPosition() const
+    {
+        return mPosition;
+    }
+
+    float getAngle() const
+    {
+        if ( glm::length2( mVelocity ) > 0.001 ) {
+            mAngle = std::atan2( mVelocity.y, mVelocity.x );
+        }
+        return mAngle;
+    }
+
+    void moveToNextSegment();
+    float calcSlowingDistance();
+    ci::vec2 truncate( ci::vec2 v, float s );
+    void findRadius( const ci::vec2 &v1, const ci::vec2 &v2, const ci::vec2 &v3, float &r, ci::vec2 &center );
+
+  protected:
+    const float mTurnDistance = 10;
+
+    float mMaxSpeed;
+    float mMaxForce;
+
+    ci::PolyLine2f mPath;
+    size_t mPrevPoint;
+    size_t mCurrPoint;
+    size_t mNextPoint;
+
+    ci::ColorA mColor;
+    ci::vec2 mPosition;
+    ci::vec2 mVelocity;
+    float mMass;
+    mutable float mAngle;
+    float mNextTurnRadius;
+    float mNextTurnSpeed;
+    float mSlowingDistance;
+};
---
+++ src/Vehicle.cpp
@@ -0,0 +1,178 @@
+//
+//  Vehicle.cpp
+//  Cityscape
+//
+//  Created by andrew morton on 7/3/16.
+//
+//
+
+#include "Vehicle.h"
+
+using namespace ci;
+
+#include <boost/format.hpp>
+#include <glm/gtx/vector_angle.hpp>
+// TODO:
+// - interpolate the rotation angle turning
+// - only closed polylines should loop
+// - build path from highways in city
+// - use instanced drawing
+// - move into the citydata structure
+// - create a car model
+
+void Vehicle::setup( const PolyLine2f &path )
+{
+    mPath = path;
+    // If it's closed and the front and back aren't the same point add it
+    // to close it up.
+    if ( path.isClosed() && glm::distance2( mPath.getPoints().front(), mPath.getPoints().back() ) > 0.001 ) {
+        mPath.getPoints().push_back( mPath.getPoints().front() );
+    }
+    mAngle = 0;
+
+    mPosition = vec2( mPath.getPoints()[0] );
+    mVelocity = vec2( 0 );
+    mMaxSpeed = 4;
+    mMaxForce = 0.5;
+    mMass = 5;
+
+    mPrevPoint = mPath.size() > 1 ? mPath.size() - 1 : 0;
+    mCurrPoint = 0;
+    mNextPoint = mPath.size() > 1 ? 1 : 0;
+    moveToNextSegment();
+}
+
+void Vehicle::moveToNextSegment()
+{
+    mPrevPoint = mCurrPoint;
+    mCurrPoint = mNextPoint;
+    ++mNextPoint;
+
+    if ( mNextPoint > mPath.size() - ( mPath.isClosed() ? 2 : 1 ) ) {
+        mNextPoint = 0;
+    }
+
+    std::vector<vec2> &points = mPath.getPoints();
+
+    vec2 p = glm::normalize( points[mCurrPoint] - points[mPrevPoint] );
+    vec2 n = glm::normalize( points[mNextPoint] - points[mCurrPoint] );
+    float diff = glm::angle( n, p );
+    if ( diff <= M_PI_4 + 0.1 ) {
+        mNextTurnSpeed = mMaxSpeed;
+    } else if ( diff <= M_PI_2 + 0.1 ) {
+        mNextTurnSpeed = mMaxSpeed / 2;
+    } else {
+        mNextTurnSpeed = mMaxSpeed / 3;
+    }
+
+    vec2 _center;
+    findRadius( points[mPrevPoint], points[mCurrPoint], points[mNextPoint], mNextTurnRadius, _center );
+
+    mSlowingDistance = calcSlowingDistance();
+}
+
+float Vehicle::calcSlowingDistance()
+{
+    float max_accel = mMaxForce / mMass;
+    float fudge_factor = 1.1;
+    return fudge_factor * std::abs( mMaxSpeed * mMaxSpeed - mNextTurnSpeed * mNextTurnSpeed ) / ( 2.0 * max_accel );
+}
+
+vec2 Vehicle::truncate( vec2 v, float s )
+{
+    if ( glm::length2( v ) >  s * s )
+        return glm::normalize( v ) * s;
+    return v;
+}
+
+// All this is based of Reynold's steering behaviors
+// http://www.red3d.com/cwr/steer/gdc99/
+void Vehicle::update( double dt )
+{
+    if ( mPath.size() < 2 ) return;
+
+    vec2 target = mPath.getPoints()[mCurrPoint];
+
+    // steer to arrive at next point
+    vec2 target_offset = target - mPosition;
+    float distance = glm::length( target_offset );
+    float ramped_speed = mNextTurnSpeed * ( distance / mSlowingDistance );
+    float clipped_speed = std::min( ramped_speed, mMaxSpeed );
+    vec2 desired_velocity = (clipped_speed / distance) * target_offset;
+    vec2 steering_direction = desired_velocity - mVelocity;
+    vec2 steering_force = truncate( steering_direction, mMaxForce );
+
+    vec2 acceleration = steering_force / mMass;
+    mVelocity = truncate( mVelocity + acceleration, mMaxSpeed );
+    mPosition = mPosition + mVelocity;
+
+    if ( ramped_speed < mMaxSpeed )
+        mColor = ColorA(1, 0, 0, 0.75);
+//        else if ( accLen2 > 0.0 )
+//            mColor = ColorA(0, 1, 0, 0.75);
+    else
+        mColor = ColorA(1, 1, 1, 0.75);
+
+    // When we get close move to the next point
+    if ( distance < mNextTurnRadius && glm::length2( mVelocity ) < ( mNextTurnSpeed * mNextTurnSpeed ) ) {
+        moveToNextSegment();
+    }
+}
+
+void Vehicle::findRadius( const vec2 &v1, const vec2 &v2, const vec2 &v3, float &r, vec2 &center )
+{
+    // Put the point at the origin create two normal vectors heading
+    // to the adjacent points.
+    vec2 prev = glm::normalize( v1 - v2 );
+    vec2 next = glm::normalize( v3 - v2 );
+
+    // Find the radius of circle tangent to the vectors leaving this
+    // point d distance away.
+    /*
+                     d
+           prev <--+----+
+                   |  θ/ \
+                  r|  /   \
+                   | /h    v
+                   |/      next
+                   +
+
+        θ=inner angle/2
+        tan(θ)=r/d => r=d*tan(θ)
+        cos(θ)=d/h => h=d/cos(θ)
+
+        I love having to relearn trig for stuff like this.
+    */
+    float diff = glm::angle( prev, next ) / 2.0;
+    r = mTurnDistance * std::tan( diff );
+
+    // If we want to display the circle we'll need to locate the
+    // center which is on the hypotenuse.
+    vec2 middle = ( prev + next ) / vec2( 2 );
+    float middleAngle = std::atan2( middle.y, middle.x );
+    float h = mTurnDistance / std::cos( diff );
+    center = v2 + vec2( std::cos( middleAngle ), std::sin( middleAngle ) ) * h;
+}
+
+void Vehicle::draw() const
+{
+    {
+        gl::ScopedColor color( mColor );
+        gl::ScopedModelMatrix matrix;
+        gl::translate( getPosition() );
+        gl::rotate( getAngle() );
+        gl::drawVector( vec3( 0, 0, 0 ), vec3( 10, 0, 0 ), 20, 10);
+    }
+    gl::draw( mPath );
+
+    // Text debugging info
+    {
+        gl::ScopedMatrices mat;
+        gl::setMatricesWindow( cinder::app::getWindowSize() );
+        boost::format formatter( "%07.5f" );
+        gl::drawString( "Speed: " + (formatter % glm::length(mVelocity)).str(), vec2( 10, 10 ) );
+        gl::drawString( "Turn:  " + (formatter % mNextTurnSpeed).str(), vec2( 10, 25 ) );
+        gl::drawString( "Max:   " + (formatter % mMaxSpeed).str(), vec2( 10, 40 ) );
+        gl::drawString( "Dist:  " + (formatter % mSlowingDistance).str(), vec2( 10, 55 ) );
+    }
+}
